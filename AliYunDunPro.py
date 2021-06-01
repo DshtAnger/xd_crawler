59124e6ca03346734ade480323686b1f1c8ed932
asuraaxu <asuraaxu@tencent.com>
2021-06-01 18:58:19 +0800

    new modify

--- AliYunDunPro.py
+++ AliYunDunPro.py
@@ -2,21 +2,21 @@
 
 import os
 
-zb_xx = 'python3 -u /root/xd/zb_xx.py >> /root/xd/log/zb_xx.log 2>&1'
+zb_xx = 'python3 -u /root/xd_crawler/zb_xx.py >> /root/xd_crawler/log/zb_xx.log 2>&1'
 
-zb_zplb = 'python3 -u /root/xd/zb_zplb.py >> /root/xd/log/zb_zplb.log 2>&1'
-zb_zplb_pl = 'python3 -u /root/xd/zb_zplb_pl.py >> /root/xd/log/zb_zplb_pl.log 2>&1'
-zb_zplb_qsbx = 'python3 -u /root/xd/zb_zplb_qsbx.py >> /root/xd/log/zb_zplb_qsbx.log 2>&1'
-zb_zplb_cyzhx = 'python3 -u /root/xd/zb_zplb_cyzhx.py >> /root/xd/log/zb_zplb_cyzhx.log 2>&1'
-zb_zplb_glsp = 'python3 -u /root/xd/zb_zplb_glsp.py >> /root/xd/log/zb_zplb_glsp.log 2>&1'
+zb_zplb = 'python3 -u /root/xd_crawler/zb_zplb.py >> /root/xd_crawler/log/zb_zplb.log 2>&1'
+zb_zplb_pl = 'python3 -u /root/xd_crawler/zb_zplb_pl.py >> /root/xd_crawler/log/zb_zplb_pl.log 2>&1'
+zb_zplb_qsbx = 'python3 -u /root/xd_crawler/zb_zplb_qsbx.py >> /root/xd_crawler/log/zb_zplb_qsbx.log 2>&1'
+zb_zplb_cyzhx = 'python3 -u /root/xd_crawler/zb_zplb_cyzhx.py >> /root/xd_crawler/log/zb_zplb_cyzhx.log 2>&1'
+zb_zplb_glsp = 'python3 -u /root/xd_crawler/zb_zplb_glsp.py >> /root/xd_crawler/log/zb_zplb_glsp.log 2>&1'
 
-zbjl = 'python3 -u /root/xd/zbjl.py >> /root/xd/log/zbjl.log 2>&1'
-zbjl_sx = 'python3 -u /root/xd/zbjl_sx.py >> /root/xd/log/zbjl_sx.log 2>&1'
-zbjl_splb = 'python3 -u /root/xd/zbjl_splb.py  >> /root/xd/log/zbjl_splb.log 2>&1'
+zbjl = 'python3 -u /root/xd_crawler/zbjl.py >> /root/xd_crawler/log/zbjl.log 2>&1'
+zbjl_sx = 'python3 -u /root/xd_crawler/zbjl_sx.py >> /root/xd_crawler/log/zbjl_sx.log 2>&1'
+zbjl_splb = 'python3 -u /root/xd_crawler/zbjl_splb.py  >> /root/xd_crawler/log/zbjl_splb.log 2>&1'
 
-zbjl_pl = 'python3  -u /root/xd/zbjl_pl.py >> /root/xd/log/zbjl_pl.log 2>&1'
-zbjl_zzgm = 'python3 -u /root/xd/zbjl_zzgm.py >> /root/xd/log/zbjl_zzgm.log 2>&1'
-zbjl_ll_rc = 'python3 -u /root/xd/zbjl_ll-rc-yl-fst-dz-tlsc.py >> /root/xd/log/zbjl_ll_rc.log 2>&1'
+zbjl_pl = 'python3  -u /root/xd_crawler/zbjl_pl.py >> /root/xd_crawler/log/zbjl_pl.log 2>&1'
+zbjl_zzgm = 'python3 -u /root/xd_crawler/zbjl_zzgm.py >> /root/xd_crawler/log/zbjl_zzgm.log 2>&1'
+zbjl_ll_rc = 'python3 -u /root/xd_crawler/zbjl_ll-rc-yl-fst-dz-tlsc.py >> /root/xd_crawler/log/zbjl_ll_rc.log 2>&1'
 
 '------------------------------------------------------------------------------'
 
