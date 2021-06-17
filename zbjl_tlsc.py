acf8457cc4d526df11195d8508fb0ab5c0f58e0d
asuraaxu <asuraaxu@tencent.com>
2021-06-17 20:40:17 +0800

    modify zbjl type py for daily history search condition

--- zbjl_tlsc.py
+++ zbjl_tlsc.py
@@ -44,11 +44,6 @@ headers = {
 }
 
 
-Entry_list = {
-    ' Daily ': True,
-    'History': True
-}
-
 def run_crawler_task(type, current_taks):
 
     #for type in input_type:
@@ -237,32 +232,12 @@ def run_crawler_task(type, current_taks):
     today_tlsc_count = 0
 
     if current_taks == ' Daily ':
-        update_date = lastday_date
+
+        query_cmd = "list_%s_zbjl.select().where(list_%s_zbjl.time_update.startswith('%s'),~list_%s_zbjl.time_update.endswith('History'))" % (type, type, today_date, type)
 
     elif current_taks == 'History':
 
-        all_history_data_query_cmd = "list_%s_zbjl_tlsc.select().where(list_%s_zbjl_tlsc.time_update.endswith('History')).order_by(list_%s_zbjl_tlsc.livestraming_time).limit(10)" % (type, type, type)
-        today_history_data_query_cmd = "list_%s_zbjl_tlsc.select().where(list_%s_zbjl_tlsc.time_update.endswith('History'),list_%s_zbjl_tlsc.time_update.startswith('%s')).order_by(list_%s_zbjl_tlsc.livestraming_time).limit(10)" % (type, type, type, today_date, type)
-
-        all_query_result = eval(all_history_data_query_cmd)
-        today_query_result = eval(today_history_data_query_cmd)
-
-        if bool(all_query_result):
-            # 可能是非首日,也可能是首日、但程序中断存在部分首日历史数据
-            if bool(today_query_result):
-                # 首日/非首日的第二次运行,即程序异常导致数据中断情景
-                latest_history_date = today_query_result[0].livestraming_time.split(' ')[0]
-                update_date = latest_history_date
-            else:
-                # 非首日,日更运行场景
-                latest_history_date = all_query_result[0].livestraming_time.split(' ')[0]
-                update_date = (datetime.datetime.strptime(latest_history_date, "%Y-%m-%d") + datetime.timedelta(days=-1)).strftime("%Y-%m-%d")
-        else:
-            # 首日第一次运行,数据库中没有任何带标记的历史数据，即也不存在任何今日历史数据
-            latest_history_date = lastday_date
-            update_date = (datetime.datetime.strptime(latest_history_date, "%Y-%m-%d") + datetime.timedelta(days=-1)).strftime("%Y-%m-%d")
-
-    query_cmd = "list_%s_zbjl.select().where(list_%s_zbjl.livestraming_time.startswith('%s')).order_by(list_%s_zbjl.time_update)" % (type, type, update_date, type)
+        query_cmd = "list_%s_zbjl.select().where(list_%s_zbjl.time_update.startswith('%s'),list_%s_zbjl.time_update.endswith('History'))" % (type, type, today_date, type)
 
     for one_record in eval(query_cmd):
 
@@ -344,7 +319,10 @@ def run_crawler_task(type, current_taks):
     logging.info(' '.join(['[%s]'%current_taks, type, 'zbjl_tlsc', '[ today_tlsc_count: %d ]' % today_tlsc_count, 'Done at', get_current_time()]))
     logging.info('-'*100)
 
-
+Entry_list = {
+    ' Daily ': True,
+    'History': True
+}
 for current_taks in Entry_list:
     p = Pool(3)
     for type in input_type:
