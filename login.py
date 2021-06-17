bfd9e26ae271e81b3ba1557d1843fa24ab718475
asuraaxu <asuraaxu@tencent.com>
2021-06-17 21:00:10 +0800

    modify main.py to add login process

--- login.py
+++ login.py
@@ -79,6 +79,8 @@ async def login():
     with open('/root/xd_crawler/cookie','w') as f:
         f.write(cookie)
 
+    await browser.close()
+
 if __name__ == '__main__':
     asyncio.get_event_loop().run_until_complete(login())
 
